b1d9a444a20c33270b382cc7b700d0a79f15e386
liuyulu <pursuebetter2018@163.com>
2023-10-03 21:28:10 +0800

    feat: update doc

--- stylelint/style.js
+++ stylelint/style.js
@@ -1,20 +1,22 @@
 module.exports = {
   rules: {
     // Base rules
-    indentation: 2,
-    'number-leading-zero': 'never',
-    'string-quotes': 'single',
-    'selector-max-id': 0,
-    'selector-list-comma-newline-after': 'always',
+    indentation: 2, // 使用 2 个空格缩进
+    'number-leading-zero': 'never', // 数字前不要有前导零
+    'string-quotes': 'single', // 使用单引号
+    'selector-max-id': 0, // 不允许使用 id 选择器
+    'selector-list-comma-newline-after': 'always', // 选择器列表后总是有换行
+    'declaration-block-semicolon-newline-after': 'always', // 在每个属性后要求一个换行符
+    "max-empty-lines": 1, // 类之间最大空行
     'comment-empty-line-before': [
       'always',
       { except: ['first-nested'] },
-    ],
-    'block-opening-brace-space-before': 'always',
-    'declaration-colon-space-after': 'always',
-    'declaration-colon-space-before': 'never',
-    'declaration-block-single-line-max-declarations': 1,
-    'declaration-property-value-disallowed-list': { '/^border/': ['none'] },
+    ], // 注释前总是有空行，但首个嵌套的注释除外
+    'block-opening-brace-space-before': 'always', // 块的左大括号前总是有空格
+    'declaration-colon-space-after': 'always', // 声明冒号后总是有空格
+    'declaration-colon-space-before': 'never', // 声明冒号前不要有空格
+    'declaration-block-single-line-max-declarations': 1, // 声明块中的声明数最多为 1 条
+    'declaration-property-value-disallowed-list': { '/^border/': ['none'] }, // 不允许使用特定值的声明
     // 不要使用已被 autoprefixer 支持的浏览器前缀
     'media-feature-name-no-vendor-prefix': true,
     'at-rule-no-vendor-prefix': true,
